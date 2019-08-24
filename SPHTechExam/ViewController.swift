2ba5a886231a5efa5c4ce711b7d02a5aea7d244b
Erica Caber <erica.caber@opsolutions.biz>
2019-08-24 12:47:34 +0800

    Initial Commit

---
+++ SPHTechExam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPHTechExam
+//
+//  Created by Erica on 24/08/2019.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
